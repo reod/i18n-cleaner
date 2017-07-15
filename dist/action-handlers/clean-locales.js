e15f190e2fa359366225cdd4e62f8cf29dd771ba
Remek Ambroziak <remek.ambroziak@gmail.com>
2017-07-15 10:21:51 +0200

    Add playground

--- dist/action-handlers/clean-locales.js
+++ dist/action-handlers/clean-locales.js
@@ -4,13 +4,12 @@ const CleanLocales_1 = require("./../use-cases/clean-locales/CleanLocales");
 const Command_1 = require("./../use-cases/clean-locales/Command");
 const FileSystemService_1 = require("./../services/file-system/FileSystemService");
 const CleaningService_1 = require("./../services/cleaning/CleaningService");
-function cleanLocales(directory, options) {
-    directory = directory || './';
+function cleanLocales(directory, refFile, options) {
     const fillMissing = options.fillMissing || false;
     const sort = options.sort || false;
     const save = options.save || false;
     const cleanLocales = new CleanLocales_1.CleanLocales(new FileSystemService_1.FileSystemService(), new CleaningService_1.CleaningService());
-    const command = new Command_1.Command(directory, fillMissing, sort, save);
+    const command = new Command_1.Command(directory, refFile, fillMissing, sort, save);
     cleanLocales.execute(command, {
         localesCleaned() {
             console.log('cleaned...');
